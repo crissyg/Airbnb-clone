6de604209f7e19b983cef4dc31d36c3e6f070f6f
Christina Gordon <crissyg@users.noreply.github.com>
2024-11-27 02:01:49 -0500

    Updated Banner
    Updated Banner .css and .js

--- src/Banner.js
+++ src/Banner.js
@@ -4,6 +4,10 @@ import './Banner.css';
 function Banner() {
   return (
     <div className='banner'>
+      <div className='banner_info'>
+        <h1>Get out and stretch your feet</h1>
+        <h5>Plan a different kind of getaway</h5>
+      </div>
       <h1>Banner</h1>
     </div>
   )
