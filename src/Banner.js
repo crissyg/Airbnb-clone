6bd30276d9ca057575549d3c6b150280e789396e
Christina Gordon <crissyg@users.noreply.github.com>
2024-11-27 02:31:03 -0500

    Updated Banner search buttons
    Updated Banner search buttons and css

--- src/Banner.js
+++ src/Banner.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState} from 'react';
+import { Button } from '@mui/material';
 import './Banner.css';
 
 function Banner() {
+    const [showSearch, setShowSearch]= useState (false);
+
   return (
     <div className='banner'>
+      <div className='banner_search'> {showSearch && <h1> SHOW DATE PICKER</h1>}
+        <Button onClick={()=>setShowSearch(!showSearch)} className='banner_searchButton' variant='outlined'> Search Dates
+        </Button>
+      </div>
       <div className='banner_info'>
         <h1>Get out and stretch your feet</h1>
         <h5>Plan a different kind of getaway</h5>
+        <Button variant='outlined'> Explore Nearby
+        </Button>
       </div>
-      <h1>Banner</h1>
     </div>
   )
 }
