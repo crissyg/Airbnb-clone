ec501aa6a206a01734af40abfed223cc1874518a
Christina Gordon <crissyg@users.noreply.github.com>
2024-11-27 01:27:47 -0500

    Updated Header and Home. Added Banner. Edited dependencies

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Avatar } from '@mui/material';
 import './Header.css';
+
 
 function Header() {
   return (
@@ -8,9 +13,26 @@ function Header() {
         src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png" 
         alt='Header Icon'
         />
+      <div className='header_center'>
+        <input type="text" />
+        <SearchIcon />
+      </div>
+
+      <div className='header_right'>
+          <p>Become a host</p>
+          <LanguageIcon />
+          <ExpandMoreIcon />
+          <Avatar />
+
+      </div>  
+
+ 
+
+    
 
         
     </div>
+    
   )
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './Home.css';
+import Banner from './Banner';
+
+//ES7 snippets to fo 'rfce'
 
 function Home() {
   return (
     <div className = 'home'> 
         <h1>Home component</h1>
+        <Banner />
     </div>
   )
+
+
 }
 
 export default Home
+
+  // Add this in your component file to check for than one instance of react that 
+  // may differ from Web and Local
+
+  // require('react-dom');
+  // window.React2 = require('react');
+  // console.log(window.React1 === window.React2);
